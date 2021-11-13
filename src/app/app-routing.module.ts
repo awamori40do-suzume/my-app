2366f9ae892737f2a53a5b66d76c7be6ccaacf56
yoshida <news.com.3104@gmail.com>
2021-11-14 00:25:54 +0900

    route設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './common/product-list/product.list';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductComponent },
+  //{ path: 'detil', component: ProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
